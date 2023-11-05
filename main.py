0bddee950b065da440068d80df028c87bc88ed05
Youssef Alkhodary <82382970+UserJoo9@users.noreply.github.com>
2023-11-05 16:10:47 +0200

    update

--- main.py
+++ main.py
@@ -1,98 +1,267 @@
+import os.path
 import pygame
 from pygame.locals import *
+from threading import Thread
+import serial
+import serial.tools.list_ports
 
+supportedBoardsModel = ("Arduino Uno", "Arduino Nano", "Silicon Labs CP210x")
+
+def start_connection():
+    global links, boardConnected, MCU
+    ports = list(serial.tools.list_ports.comports())
+    attached = 0
+    port = None
+    for p in ports:
+        if any(one in p.description for one in supportedBoardsModel):
+            attached += 1
+            port = p.description
+    if attached == 1:
+        for o in supportedBoardsModel:
+            if o in port:
+                com = port.split("(")[-1][:-1]
+                MCU = serial.Serial(port=com, baudrate=9600)
+                MCU.close()
+                MCU.open()
+                boardConnected = True
+                return "Connected"
+    elif attached > 1:
+        return "Attach one MCU only!"
+    else:
+        return "No MCU board attached!"
+start_connection()
+
+# main attributes definition
 cups_levels = ['1.png', '2.png', '3.png']
-round = 1
+general_value = 0
+round = 0
 cup_number = 0
-reach = 0
 running = True
+isReady = True
+rest = False
+stillPressed = False
+grabbed = False
+half_cup = False
+full_cup = False
 size = width, height = (700, 700)
 pygame.init()
 pygame.mixer.init()
+squeeze_sound = pygame.mixer.Sound('squezee.mp3')
+bg_music_sound = pygame.mixer.Sound('bg_music.mp3')
+bg_music_sound.set_volume(min(1.0, bg_music_sound.get_volume() / 8))
+win_sound = pygame.mixer.Sound('win.mp3')
+
+# set window size
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('')
+
+# set game window name
+if os.path.exists('gn.txt'):
+    pygame.display.set_caption(open('gn.txt', 'r').read())
+else:
+    open('gn.txt', 'w').write('grip strength game')
+
+# set game window icon
 icon = pygame.image.load('orange.png')
 pygame.display.set_icon(icon)
 
+# background image init
 background = pygame.image.load('bg.jpg')
 background_loc = background.get_rect()
 background_loc.center = 350, 300
 
+# main object init (Orange)
 main_object = pygame.image.load('orange.png')
 main_object_loc = main_object.get_rect()
 main_object_loc.center = width-210, height/2
 res_main_object = pygame.transform.scale(main_object, (320, 220))
 
+# cup image init
 cup_loc = main_object.get_rect()
 cup_loc.center = width-200, height-150
 res_cup = pygame.transform.scale(pygame.image.load(cups_levels[0]), (300, 360))
 
+# table image
+table = pygame.image.load('table.png')
+table_loc = table.get_rect()
+table_loc.center = width / 18, height - 150
+res_table = pygame.transform.scale(table, (1200, 400))
+
+# rounds counter init
 font = pygame.font.Font(None, 36)
 text_surface = font.render(f"Round {round}", True, (255, 255, 255))
 text_surface_loc = text_surface.get_rect()
 text_surface_loc.center = 60, 30
 
+# cups counter init
 cups_text_surface = pygame.font.Font(None, 36).render(f"Cups {cup_number}", True, (255, 255, 255))
 cups_text_surface_loc = cups_text_surface.get_rect()
 cups_text_surface_loc.center = 650, 30
 
-end_text = pygame.font.Font(None, 140).render("Good job.", True, (0, 255, 0))
+# controller navigate
+controller_nav = pygame.font.Font(None, 36).render(f"Controller connecting..", True, (255, 140, 0))
+controller_nav_loc = controller_nav.get_rect()
+controller_nav_loc.center = width / 2, 30
+
+# break init (Rest)
+rest_text = pygame.font.Font(None, 150).render(f"Take a rest.", True, (0, 0, 255))
+rest_text_loc = rest_text.get_rect()
+rest_text_loc.center = 350, 350
+
+# endgame init (Win)
+end_text = pygame.font.Font(None, 150).render("Good job", True, (0, 255, 0))
 end_text_loc = end_text.get_rect()
 end_text_loc.center = 350, 350
 
-def sound(sound):
-    pygame.mixer.music.stop()
-    pygame.mixer.music.unload()
-    pygame.mixer.music.load(sound)
-    pygame.mixer.music.play()
+# update all objects on screen
+def update_all_objects():
+    screen.blit(background, background_loc)
+    screen.blit(res_main_object, main_object_loc)
+    screen.blit(res_table, table_loc)
+    screen.blit(res_cup, cup_loc)
+    screen.blit(text_surface, text_surface_loc)
+    screen.blit(cups_text_surface, cups_text_surface_loc)
+    screen.blit(controller_nav, controller_nav_loc)
+    pygame.display.update()
 
+# move full cup animation and get new empty cup
+def next_cup():
+    global cup_loc, main_object_loc, res_cup, cup, isReady
+    pygame.time.delay(200)
+    isReady = False
+    while cup_loc[0] < width and main_object_loc[0] < width:
+        cup_loc = cup_loc.move(100, 0)
+        main_object_loc = main_object_loc.move(100, 0)
+        pygame.display.update()
+        pygame.time.delay(50)
+    cup = pygame.image.load(cups_levels[0])
+    res_cup = pygame.transform.scale(cup, (300, 360))
+    cup_loc.center = 0, height-150
+    main_object_loc.center = 0, height/2
+    orange()
+    while cup_loc[0] < width / 2 - 200 and main_object_loc[0] < width / 2 - 200:
+        cup_loc = cup_loc.move(100, 0)
+        main_object_loc = main_object_loc.move(100, 0)
+        pygame.display.update()
+        pygame.time.delay(50)
+    isReady = True
+
+# view orange on screen
+def orange():
+    global main_object, res_main_object
+    main_object = pygame.image.load('orange.png')
+    res_main_object = pygame.transform.scale(main_object, (320, 220))
+
+# view squeezed orange on screen
+def squezeed_orange():
+    global main_object, res_main_object
+    main_object = pygame.image.load('squezeed.png')
+    res_main_object = pygame.transform.scale(main_object, (320, 600))
+
+# view ended orange on screen
+def ended_orange():
+    global main_object, res_main_object
+    main_object = pygame.image.load('ended_orange.png')
+    res_main_object = pygame.transform.scale(main_object, (320, 600))
+
+# take rest function
+def take_rest():
+    global isReady, rest_text, rest_text_loc, rest
+    isReady = False
+    rest = True
+    screen.blit(rest_text, rest_text_loc)
+    pygame.display.update()
+    pygame.time.delay(1000 * 60)
+    isReady = True
+    rest = False
+
+# check live sensor value
+def active_press():
+    global stillPressed, MCU, general_value, controller_nav
+    while 1:
+        pygame.time.delay(50)
+        try:
+            general_value = int(MCU.read().decode().strip())
+            if str(general_value).isnumeric():
+                controller_nav = pygame.font.Font(None, 36).render(f"Controller connected", True, (00, 255, 0))
+
+        except:
+            controller_nav = pygame.font.Font(None, 36).render(f"Controller disconnected!", True, (255, 0, 0))
+Thread(target=active_press, daemon=True).start()
+
+# check live press
+def still_pressed():
+    global stillPressed, general_value
+    if general_value >= 1:
+        stillPressed = True
+    else:
+        stillPressed = False
+
+# game core
 while running:
+    if not pygame.mixer.get_busy():
+        bg_music_sound.play()
+
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
 
-        if event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                reach += 1
-                main_object = pygame.image.load('squezeed.png')
-                res_main_object = pygame.transform.scale(main_object, (320, 600))
-                if reach == 1:
-                    cup = pygame.image.load(cups_levels[reach])
-                    res_cup = pygame.transform.scale(cup, (300, 360))
-                    sound('squezee.mp3')
-                elif reach == 2:
-                    cup = pygame.image.load(cups_levels[reach])
-                    res_cup = pygame.transform.scale(cup, (300, 360))
-                    cup_number += 1
-                    cups_text_surface = font.render(f"Cups {cup_number}", True, (255, 255, 255))
-                    sound('squezee.mp3')
+    if not grabbed:
+        grabbed = True
+        bypass = False
+        for x in range(60):
+            pygame.time.delay(50)
+            still_pressed()
+            if stillPressed:
+                bypass = True
+            else:
+                break
 
-        if event.type == KEYUP:
-            main_object = pygame.image.load('orange.png')
-            res_main_object = pygame.transform.scale(main_object, (320, 220))
-            if reach == 2:
-                if cup_number < 5:
-                    reach = 0
-                cup = pygame.image.load(cups_levels[0])
+        if bypass:
+            if general_value == 1 and not half_cup:
+                half_cup = True
+                squezeed_orange()
+                cup = pygame.image.load(cups_levels[1])
                 res_cup = pygame.transform.scale(cup, (300, 360))
-                if cup_number == 5:
-                    main_object = pygame.image.load('orange.png')
-                    res_main_object = pygame.transform.scale(main_object, (320, 220))
-                    cup_number = 0
-                    reach = 0
-                    round += 1
-                    text_surface = font.render(f"Round {round}", True, (255, 255, 255))
-                    if round == 3:
-                        sound('win.mp3')
+                squeeze_sound.play()
+            elif general_value == 2 and half_cup:
+                full_cup = True
+                squezeed_orange()
+                cup = pygame.image.load(cups_levels[2])
+                res_cup = pygame.transform.scale(cup, (300, 360))
+                cup_number += 1
+                cups_text_surface = font.render(f"Cups {cup_number}", True, (255, 255, 255))
+                squeeze_sound.play()
 
-    screen.blit(background, background_loc)
-    screen.blit(res_main_object, main_object_loc)
-    screen.blit(res_cup, cup_loc)
-    screen.blit(text_surface, text_surface_loc)
-    screen.blit(cups_text_surface, cups_text_surface_loc)
+    still_pressed()
+    if not stillPressed and isReady and not rest:
+        grabbed = False
+        squeeze_sound.stop()
+        orange()
+        if full_cup:
+            ended_orange()
+            half_cup = False
+            full_cup = False
+            if isReady:
+                Thread(target=next_cup, daemon=True).start()
+            if cup_number == 5:
+                cup_number = 0
+                round += 1
+                text_surface = font.render(f"Round {round}", True, (255, 255, 255))
+                cups_text_surface = font.render(f"Cups {cup_number}", True, (255, 255, 255))
+                if round == 3:
+                    win_sound.play()
+                else:
+                    Thread(target=next_cup, daemon=True).start()
+                    Thread(target=take_rest, daemon=True).start()
+
     if round == 3:
         screen.blit(end_text, end_text_loc)
         pygame.display.update()
+        bg_music_sound.fadeout(2)
         pygame.time.delay(3000)
-        pygame.quit()
-    pygame.display.update()
+        running = False
+
+    if not rest:
+        update_all_objects()
+
+pygame.quit()
